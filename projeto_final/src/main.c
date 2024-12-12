e1cb9e6d02dbd4d0178a69dd458f0fe44df69a9f
Jonathan Cardozo Maciel <cardozo@ieee.org>
2024-12-12 11:09:49 -0300

    Update main.c
    Ajuste para realizar a leitura gráfica

--- projeto_final/src/main.c
+++ projeto_final/src/main.c
@@ -18,26 +18,26 @@ int main(void) {
     const struct device *i2c_dev = DEVICE_DT_GET(DT_NODELABEL(I2C_DEV_NAME));
 
     if (i2c_dev == NULL || !device_is_ready(i2c_dev)) {
-        printf("Could not get I2C device\n");
+        printf("Não foi possível iniciar o dispositivo I2C!\n");
         return 0;
     }
 
-    printf("Sensor ready! Starting readings:\n");
+    printf("Sensor pronto! Início da leitura:\n");
 
     for (;;) {
         uint8_t temperature;
         int ret = i2c_reg_read_byte(i2c_dev, AT30TSE75X_ADDR, AT30TSE75X_REG_REA, &temperature);
 
         if (ret != 0) {
-            printf("I2C read error: %d\n", ret);
+            printf("Erro leitura I2C: %d\n", ret);
             k_sleep(SLEEP_TIME);
             continue;
         }
 
         if (temperature > 127 || temperature < -128) {
-            printf("Invalid temperature reading: %d\n", temperature);
+            printf("Leitura inválida: %d\n", temperature);
         } else {
-            printf("Temperature: %d C\n", temperature);
+		printf("Temperatura: %d C\n", temperature);
         }
 
         k_sleep(SLEEP_TIME);
